ebd04a6afab761097dfad1501418a7cfc4fce855
thundersmotch <2165764+ThunderSmotch@users.noreply.github.com>
2020-06-04 01:36:34 +0100

    Finished button with cooldown and added tooltips

--- js/elements/button.js
+++ js/elements/button.js
@@ -1,10 +1,33 @@
 export var Button = {
-    init: function(id){
+    init: function(){
 
     },
     create: (id) => getButtonHTML(id),
+
+    createCooldownButton: (id, cd) => createCooldownButton(id, cd)
 }
 
 function getButtonHTML(id){
-    return `<div class='button' id='${id}'>Test Button</div>`;
+    return `<div class='button' id='${id}'>Test Button<div class='buttonBG'><div></div>`;
+}
+
+//Creates a button of the type Cooldown (has a BG cooldown bar)
+function createCooldownButton(id, cd){
+
+    var newButton = $(Button.create(id)).click(function(){
+        
+        var button = $(this);
+        var buttonBG = $(this).children().eq(0);
+
+        buttonBG.css('animation-duration', cd);
+        button.addClass('disabled');
+        
+        buttonBG.on("animationend webkitAnimationEnd", function(){
+            if(button.hasClass('disabled'))    
+                button.removeClass('disabled');
+        });
+
+    });
+
+    return newButton;
 }
--- js/game.js
+++ js/game.js
@@ -1,12 +1,17 @@
 import {Footer} from './elements/footer.js';
 import {Button} from './elements/button.js';
 
+
 let version = "v0.1";
 
 window.onload = function(){
     Footer.init(version);
     
-    $('.main').append(Button.create('testButton')).click(function(){
-        console.log("xd");
+    var testButton = Button.createCooldownButton('test', '0.5s')
+
+    $('.main').append(testButton);
+
+    tippy('#test', {
+        content: 'Test Tooltip'
     });
 }
