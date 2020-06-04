01133c3af9979074b22d9fb461d28fa47eda59f9
thundersmotch <2165764+ThunderSmotch@users.noreply.github.com>
2020-06-04 12:43:48 +0100

    Very very early implementation of localization

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+import {Lang} from './lang.js';
 import {Footer} from './elements/footer.js';
 import {Button} from './elements/button.js';
 import {Tooltip} from './elements/tooltip.js';
@@ -8,17 +9,20 @@ let version = "v0.1";
 //TODO: Storage Localization  Save/Export/Load Game Achievements
 
 window.onload = function(){
+    Lang.init();
+    window._ = Lang.translate;
+
     Footer.init(version);
     Chat.init();
 
     setTooltipDefaultProps();
 
-    var testButton = Button.createCooldownButton('test', 'Gather plants', '0.5s');
+    var testButton = Button.createCooldownButton('test', _('button.gatherHerbs'), '0.5s');
 
     $('#mainCol').append(testButton);
 
     testButton.click(()=>{
-        Chat.addMessage('You gather some herbs...');
+        Chat.addMessage(_('chat.gatherHerbs'));
     });
 
     Tooltip.addTooltip('#test', 'Gather some W33D')
---
+++ js/lang.js
@@ -0,0 +1,40 @@
+export var Lang = {
+    init: () => init(),
+    loadLang: (lang) => loadLang(lang),
+    translate: (key) => translate(key),
+}
+
+//Available Languages
+var langs = ["en", "pt"];
+
+var currentLang = "en";
+var langJSON;
+
+//Initializes the Lang class with default parameters
+function init(){
+    loadLang("en");
+}
+
+//Loads a new language file
+//TODO: Synchronous requests are deprecated. Handle this.
+function loadLang(lang){
+    if(langs.includes(lang)){
+        $.ajax(`/lang/${lang}.lang`,{
+            async: false,
+            success: (data) => { langJSON = JSON.parse(data); },
+            error: () => {console.log("ERROR: Could not load language.")}
+        })
+    } else {
+        console.log(`ERROR: Language ${lang} not detected!`);
+    }
+}
+
+//Translates a key to string and returns it
+function translate(key){
+    try {
+        return langJSON[key];
+    } catch (error) {
+        console.log("ERROR: Could not translate key!")
+    }
+}
+
